a806ac78cbb9ddfab2fac20b1cbb01e29539a32e
Adeel Siddiqui <unknownguytoday@gmail.com>
2019-05-26 17:52:17 +0800

    Add first Redux flow

---
+++ src/redux/reducers/courseReducer.js
@@ -0,0 +1,10 @@
+import * as types from "../actions/actionTypes";
+
+export default function createReducer(state = [], action) {
+  switch (action.type) {
+    case types.CREATE_COURSE:
+      return [...state, { ...action.course }];
+    default:
+      return state;
+  }
+}
